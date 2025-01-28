bf9f56415e8dec329dacb60a04eb1eb1e0a604d8
Henrique Florencio <henrique.florencio307@gmail.com>
2025-01-27 21:08:54 -0300

    fix: snapping motion state

--- src/App.tsx
+++ src/App.tsx
@@ -1,6 +1,6 @@
 import useEmblaCarousel from "embla-carousel-react";
-import { useEffect, useState } from "react";
-import { DragControls, Reorder, useDragControls } from "motion/react";
+import { useCallback, useState } from "react";
+import { Reorder, useDragControls } from "motion/react";
 
 function App() {
   const [emblaActive, setEmblaActive] = useState(true);
@@ -11,50 +11,64 @@ function App() {
     watchDrag: emblaActive,
   });
 
+  const scrollPrev = useCallback(() => {
+    if (emblaApi) emblaApi.scrollPrev();
+  }, [emblaApi]);
+
+  const scrollNext = useCallback(() => {
+    if (emblaApi) emblaApi.scrollNext();
+  }, [emblaApi]);
+
   function handleEmblaState() {
     setEmblaActive((state) => !state);
   }
 
-  useEffect(() => {
-    if (emblaApi) {
-      console.log(emblaApi.slideNodes()); // Access API
-    }
-  }, [emblaApi]);
-
   return (
-    <div>
-      <Reorder.Group
-        values={itens}
-        onReorder={setItens}
-        axis="x"
-        // className="flex gap-5 select-none"
-        ref={emblaRef}
-      >
-        <div className="flex gap-5 select-none">
-          {itens.map((item, index) => (
-            <SlideItem value={item} index={index} key={item} />
-          ))}
-        </div>
-      </Reorder.Group>
-    </div>
+    <Reorder.Group
+      values={itens}
+      onReorder={setItens}
+      axis="x"
+      ref={emblaRef}
+    >
+      <div className="flex gap-5 select-none">
+        {itens.map((item) => (
+          <Item item={item} key={item} handleEmblaState={handleEmblaState} />
+        ))}
+      </div>
+      {/* <div
+          className="embla__prev absolute top-0 h-full bg-slate-900 opacity-45 w-5"
+          onMouseEnter={emblaActive ? undefined : scrollPrev}
+        ></div>
+        <div
+          className="embla__next absolute top-0 h-full bg-slate-900 opacity-45 w-5 right-0"
+          onMouseEnter={emblaActive ? undefined : scrollNext}
+        ></div> */}
+    </Reorder.Group>
   );
 }
 
-function SlideItem({ ...props }: any) {
+interface ItemProps {
+  item: number;
+  handleEmblaState: () => void;
+}
+
+function Item({ item, handleEmblaState }: ItemProps) {
   const controls = useDragControls();
 
   return (
-    <Reorder.Item {...props} dragListener={false} dragControls={controls}>
+    <Reorder.Item
+      value={item}
+      dragListener={false}
+      dragControls={controls}
+    >
       <div className="m-5 bg-slate-200 rounded-md p-3 aspect-256/383 min-w-[200px]">
         <div className="flex justify-between items-center mb-5">
-          <h1 className="text-lg bold">
-            Item {props.value} - index {props.index}
-          </h1>
+          <h1 className="text-lg bold">Item {item}</h1>
           <div
-            // onMouseEnter={handleEmblaState}
-            // onMouseOut={handleEmblaState}
+            onMouseEnter={handleEmblaState}
+            onMouseOut={handleEmblaState}
             className="w-5 h-5 bg-amber-400 cursor-grab reorder-handle"
-            onPointerDown={(e) => {e.preventDefault();controls.start(e)}}
+            onPointerDown={(e) => controls.start(e)}
           />
         </div>
         <article className="text-sm text-gray-800 line-clamp-8 bold">
